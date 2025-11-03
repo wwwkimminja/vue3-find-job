618528e7c85ceb8f760a8a1ba2efc1f71fd4330c
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2025-11-03 17:32:11 +0900

    feature log out

--- src/view/UserProfile.vue
+++ src/view/UserProfile.vue
@@ -6,6 +6,16 @@ import { ref } from 'vue';
 
 const router=useRouter()
 const isLogin = ref(false)
+
+const handleLogout=async()=>{
+  const { error } = await supabase.auth.signOut()
+  if(error){
+alert('Failed log out')
+  }else{
+alert('logged out')
+router.push('/')
+  }
+}
 onMounted(async()=>{
 const { data: { user } } = await supabase.auth.getUser()
   console.log(user?.email)
@@ -23,13 +33,25 @@ const { data: { user } } = await supabase.auth.getUser()
 
 <template>
   <div class="container" v-if="isLogin">
-    <h1>User Profile</h1>
+   <div class="form-container">
+    <button class="logout" @click="handleLogout">Log out</button>
+   </div>
 
   </div>
 
 </template>
 
 <style scoped lang="scss">
+  button { 
+    background: transparent;
+    color: var(--main-color);
+    font-size: 16px;
+    margin-top: 40px;
+    &:hover{
+      opacity: 0.7;
+      text-decoration: underline;
+    }
+  }
 
 
 </style>
