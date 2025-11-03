e8df2688bed6e1f1b18881709a0df1ae09b47328
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2025-11-03 17:24:07 +0900

    feature login-access

--- src/view/UserProfile.vue
+++ src/view/UserProfile.vue
@@ -1,9 +1,32 @@
 <script setup>
+import supabase from '../supabase';
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import { ref } from 'vue';
 
+const router=useRouter()
+const isLogin = ref(false)
+onMounted(async()=>{
+const { data: { user } } = await supabase.auth.getUser()
+  console.log(user?.email)
+
+  if(user){
+ console.log('logged in')
+ isLogin.value=true
+  }else{
+    alert('Available after logging in')
+    router.push('/')
+  }
+
+})
 </script>
 
 <template>
-<h1>User Profile</h1>
+  <div class="container" v-if="isLogin">
+    <h1>User Profile</h1>
+
+  </div>
+
 </template>
 
 <style scoped lang="scss">
