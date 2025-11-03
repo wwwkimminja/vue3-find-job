4b0deb9f35478311e76f6609188e0d9f81fd298a
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2025-11-03 11:57:49 +0900

    style login page

--- src/view/Login.vue
+++ src/view/Login.vue
@@ -1,12 +1,72 @@
 <script setup>
+import {ref} from 'vue'
 
+const email=ref('')
+const password=ref('')
+const handleLogin=()=>{
+  console.log(email.value,password.value)
+}
 </script>
 
 <template>
 <h1>Login</h1>
+<div class="form-container">
+  <form @submit.prevent="handleLogin">
+    <div class="form-group">
+      <label for="email" class="email">Email</label>
+      <input type="email" id="email" placeholder="enter email" required v-model="email"/>
+    </div>
+    <div class="form-group">
+      <label for="password" class="password">password</label>
+      <input type="password" id="password" placeholder="enter password" required v-model="password"/>
+    </div>
+    <button type="submit">Login</button>
+    <router-link to="/signup">create account</router-link>
+  </form>
+</div>
+
 </template>
 
 <style scoped lang="scss">
+.form-container {
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 15px;
+  margin-top: 80px;
+  background-color: #fff;
+}
 
+.form-group {
+  margin-bottom: 24px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+  font-weight: bold;
+}
+
+input {
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+button {
+  width: 100%;
+  padding: 10px;
+  background-color: var(--main-color-dark);
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-bottom: 24px;
+}
+
+button:hover {
+  opacity: 0.8;
+}
 
 </style>
