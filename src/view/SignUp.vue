f0a36a974b00e7b6b84bab9eff70035605f44bb6
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2025-11-03 15:04:22 +0900

    add sign up

--- src/view/SignUp.vue
+++ src/view/SignUp.vue
@@ -1,12 +1,44 @@
 <script setup>
+import {ref} from 'vue'
 
+const email=ref('')
+const password=ref('')
+const tel=ref('')
+const text=ref('')
+const handleSignup=()=>{
+  console.log(email.value,password.value,tel.value,text.value)
+}
 </script>
 
 <template>
-<h1>Sign Up</h1>
+<div class="form-container">
+  <form @submit.prevent="handleSignup">
+    <div class="form-group">
+      <label for="email" class="email">Email</label>
+      <input type="email" id="email" placeholder="enter email" required v-model="email"/>
+    </div>
+    <div class="form-group">
+      <label for="password" class="password">Password</label>
+      <input type="password" id="password" placeholder="enter password" required v-model="password"/>
+    </div>
+    <div class="form-group">
+      <label for="tel" class="tel">Tel</label>
+      <input type="tel" id="tel" placeholder="080-123-1234" required v-model="tel"/>
+    </div>
+    <div class="form-group">
+      <label for="text" >Career</label>
+      <textarea type="text" id="text"  required v-model="text"/>
+    </div>
+    <button type="submit">Sign up</button>
+ 
+  </form>
+</div>
 </template>
 
-<style scoped lang="scss">
 
+
+<style lang="scss">
+
+  @use "../style/form.scss"; 
 
 </style>
